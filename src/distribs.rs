85ac5617ba41f0d8de441fd17df8da8bf82ecbd1
zach <zachkdavis00@gmail.com>
2024-02-01 15:36:10 -0500

    started using bessel from library

--- src/distribs.rs
+++ src/distribs.rs
@@ -1,5 +1,7 @@
 use ndarray::{Array1, ArrayView1, s, Zip};
 use std::f64::consts::E;
+use scilib::math::bessel;
+use num::complex::Complex64;
 use crate::constants::*;
 use crate::SRtoolkit::srtoolkit::*;
 use crate::misc::*;
@@ -33,7 +35,7 @@ pub fn eq_59_park1995(t: f64, g: ArrayView1<f64>) -> Array1<f64> {
     let gfb = x.mapv(|x_i| x_i.powf((1.0 - q + a) / 2.0)) * x0.powf((1.0 - q - a) / 2.0);
     let etta = (q - 1.0 + a) / (2.0 * alpha);
     let order = etta.abs();
-    let gfc = x.mapv(|x_i| bessel_in(order, (x_i.powf(alpha) * x0.powf(alpha)) / (2.0 * tau * alpha.powi(2))));
+    let gfc = x.mapv(|x_i| bessel::i_nu(order, Complex64::new((x_i.powf(alpha) * x0.powf(alpha)) / (2.0 * tau * alpha.powi(2)), 0.0)).re);
     let gfd = x.mapv(|x_i| (-((x_i.powf(2.0 * alpha)) + x0.powf(2.0 * alpha)) / (4.0 * alpha.powi(2) * tau)).exp());
     let gfe = (-theta * tau).exp();
 
