7a1ad0eea98cd015c74d132b25101cb3279878a1
zkdavis <zachkdavis00@gmail.com>
2024-02-21 00:47:14 -0500

    working on compton test

--- tests/test_dependencies/radiation_test.py
+++ tests/test_dependencies/radiation_test.py
@@ -1,53 +1,82 @@
 import numpy as np
 import typing
-import constants as C
+from pysrc import constants as C
+import paramo as para
 
 
 
-def u_black_body(eps,sig):
+def clamp(n, min, max):
+    if (n < min):
+        return min
+    elif (n > max):
+        return max
+    else:
+        return n
+
+def clamp_arr(n, min, max):
+    for i in range(len(n)):
+        if (n[i] < min):
+            n[i] = min
+        else:
+            n[i] = max
+
+    return n
+
+def u_black_body(eps,sig,norm):
     f1 = 8*np.pi*C.me*(C.cLight**2)*(eps**3)
     lc = C.hPlanck/(C.me*C.cLight)
-    f = f1/((lc**3)*(np.exp(eps/sig) - 1 ))
+    temexp = np.exp(eps/sig)
+    for i in range(len(temexp)):
+        if temexp[i] <= 1.0000001:
+            temexp[i] = 1e200
+    f = f1/((lc**3)*(temexp- 1 ))
+    f = norm*f/np.trapz(f,eps)
     return f
 
 
 # Compton distribution function: see Dermer 6.75
-def Fc(e_s=None, g=None, e_i=None, q=None, Gam=None):
-    # have to specify args
-    # g is lorents factor, e_s is reduced scattered photon energy and e_i is reduced incoming photon energy.
-    if q is None:
-        Gam = 4 * g * e_i
-        q = e_s / (g * Gam * (1 - (e_s / g)))
-        if(q>1 or q<(1/(4*(g**2)))):
-            return 0
-        fc = 2 * q * np.log(q) + (1 + (2 * q)) * (1 - q) + (((Gam * q)**2) * (1 - q)) / (2 * (1 + (Gam * q)))
-
-        return fc
-    else:
-        fc = 2 * q * np.log(q) + (1 + (2 * q)) * (1 - q) + (((Gam * q)**2) * (1 - q)) / (2 * (1 + (Gam * q)))
+def Fc(q, Gam):
+        fc = (2 * q * np.log(q)) + ((1 + (2 * q)) * (1 - q)) + ((((Gam * q)**2) * (1 - q)) / (2 * (1 + (Gam * q))))
         return fc
 
+def g_min_f(eps_s,eps):
+    return (eps_s/2)*(1 + np.sqrt(1 + (1/(eps*eps_s))))
 
+def g_max_f(eps_s,eps,g2):
+    x = eps-eps_s
+    f = np.zeros_like(x)
+    for i in range(len(x)):
+        if(x[i]>0):
+            f[i]= eps[i]*eps_s/(eps[i] - eps_s)
+        else:
+           f[i] = g2
+    return f
 
 ##eq 6.89 dermer
-##inverse compton emission from isotropic electrons and an isotropic photon field
+##inverse compton emission from isotropic power law electrons and an isotropic photon field
 def j_ic_iso(eps_s:float,eps_array,u:typing.Callable,n_array:[float],g_array:[float]):
     fcon = (3/4)*C.cLight*C.sigmaT*eps_s
 
-    def g_int(Fc_array):
+    def g_int(Fc_array,g_min_i,g_max_i):
         #assumes g_array is logspaced
-        return np.trapz(n_array*Fc_array*np.log(10)/g_array,g_array)
+        return np.trapz(n_array[g_min_i:g_max_i]*Fc_array[g_min_i:g_max_i]*np.log(10)/g_array[g_min_i:g_max_i],np.log10(g_array[g_min_i:g_max_i]))
 
 
     g_int_array = np.zeros_like(eps_array)
+    g_max_array = g_max_f(eps_s,eps_array,g_array[-1])
+    g_min_array = g_min_f(eps_s,eps_array)
     for i in range(len(eps_array)):
         eps_i = eps_array[i]
         Gamma_i_array = 4 * g_array * eps_i
         q_i_array = eps_s / (g_array * Gamma_i_array * (1 - (eps_s / g_array)))
+        for j in range(len(q_i_array)):
+            q_i_array[j] = clamp(q_i_array[j],(4*g_array[j])**(-1),1)
         Fc_i_array = Fc(q=q_i_array,Gam=Gamma_i_array)
-        g_int_array[i] = g_int(Fc_i_array)
+        gmin_i = np.argmin(np.abs(g_array - g_min_array[i]))
+        gmax_i = np.argmin(np.abs(g_array - g_max_array[i]))
+        g_int_array[i] = g_int(Fc_i_array,gmin_i,gmax_i)
     # assumes eps_array is logspaced
-    return fcon*np.trapz(u(eps_array)*g_int_array*np.log(10)/eps_array,eps_array)
+    return fcon*np.trapz(u(eps_array)*g_int_array*np.log(10)/eps_array,np.log10(eps_array))
 
 def j_ic_iso_full(eps_s_array:[float],eps_array:[float],u:typing.Callable,n_array:[float],g_array:[float]):
     j_ic = np.zeros_like(eps_s_array)
@@ -57,22 +86,30 @@ def j_ic_iso_full(eps_s_array:[float],eps_array:[float],u:typing.Callable,n_arra
 
     return j_ic
 
-def j_ic_iso_BB(eps_s_array:[float],eps_array:[float],n_array:[float],g_array:[float],sig_T:float):
+def j_ic_iso_BB(eps_s_array:[float],eps_array:[float],n_array:[float],g_array:[float],sig_T:float,u_bb:float):
     def ub(eps):
-        return u_black_body(eps,sig_T)
+        return u_black_body(eps,sig_T,u_bb)
     j_ic = j_ic_iso_full(eps_s_array,eps_array,ub,n_array,g_array)
     return j_ic
 
 if(__name__=="__main__"):
     import matplotlib.pyplot as plt
-    p = -1
+    p = -2
     sig_T = 1
-    eps_s = np.logspace(5,30)*C.hPlanck/(C.me*(C.cLight**2))
-    eps = eps_s
-    g_array = np.logspace(1,5)
+    u_bb = 1e0
+    num_eps = 100
+    num_eps_s = 100
+    numg = 1000
+    nu_s = np.logspace(10,35,num_eps_s)
+    eps_s = nu_s*C.hPlanck/(C.me*(C.cLight**2))
+    eps =  np.logspace(12,14,num_eps)*C.hPlanck/(C.me*(C.cLight**2))
+    g_array = np.logspace(1,5,numg)
     n_array = (g_array**p)/np.trapz(g_array**p,g_array)
+    def ub(epsa):
+        return u_black_body(epsa,sig_T,u_bb)
+    j_ic = j_ic_iso_BB(eps_s,eps,n_array,g_array,sig_T,u_bb)
 
-    j_ic = j_ic_iso_BB(eps_s,eps,n_array,g_array,sig_T)
+    j_ic_para = para.ic_iso_powlaw_full(nu_s, C.cLight*ub(eps_s), g_array, n_array)
 
     fig,ax = plt.subplots()
 
@@ -80,5 +117,6 @@ if(__name__=="__main__"):
     ax.set_yscale("log")
 
     ax.plot(eps_s,j_ic)
+    ax.plot(eps_s,j_ic_para)
 
     plt.show()
