f8cf03d3caefb402502c7aaa8081fdcb310b010a
zach <zachkdavis00@gmail.com>
2024-02-22 13:30:37 -0500

    cleaned up and corrected low energies

--- pysrc/comparisons/ic_bb_test.py
+++ pysrc/comparisons/ic_bb_test.py
@@ -10,14 +10,10 @@ if __name__ == "__main__":
     sig_T = 0.0001
     Temp = sig_T*C.energy_e/C.kBoltz
     u_bb = 1e0
-    num_eps = 1000
-    num_eps_s = 1000
-    numg = 1000
+    num_eps_s = 100
+    numg = 100
     nu_s = np.logspace(8, 25, num_eps_s)
     eps_s = nu_s * C.hPlanck / (C.me * (C.cLight ** 2))
-    eps_s2 = nu_s * C.hPlanck
-    eps = np.logspace(8, 25, num_eps) * C.hPlanck / (C.me * (C.cLight ** 2))
-    eps2 = np.logspace(8, 25, num_eps) * C.hPlanck
     g_array = np.logspace(1, 3, numg)
     n_array = n0*(g_array ** -p) / np.trapz(g_array ** -p, g_array)
 
@@ -25,20 +21,22 @@ if __name__ == "__main__":
     def ub(epsa):
         return rt.BB_photon_density(epsa,Temp)*epsa*u_bb/np.trapz(rt.BB_photon_density(epsa,Temp)*epsa,epsa)
 
+    def ub_dermer(epsa):
+        epsa = epsa * C.energy_e
+        return rt.BB_photon_density(epsa, Temp) * epsa * u_bb / np.trapz(rt.BB_photon_density(epsa, Temp) * epsa, epsa)
 
-    j_ic2 = rt.j_ic_iso_BB_dermer(eps_s, eps, n_array, g_array, sig_T, u_bb)/C.energy_e
 
-    # j_ic = rt.j_ic_iso_full(eps_s2, eps_s2, u_bb,Temp,p, n0, g_array)
+    j_ic = rt.j_ic_iso_full_dermer(eps_s, eps_s,ub_dermer, n_array, g_array)#/C.energy_e
 
-    j_ic_para = np.array(para.ic_iso_powlaw_full(nu_s, C.cLight * ub(eps_s2), g_array, n_array))
+
+    j_ic_para = np.array(para.ic_iso_powlaw_full(nu_s, C.cLight * ub(nu_s*C.hPlanck), g_array, n_array))
 
     fig, ax = plt.subplots()
 
     ax.set_xscale("log")
     ax.set_yscale("log")
 
-    # ax.plot(nu_s, j_ic)
-    ax.plot(nu_s, j_ic_para)
-    ax.plot(nu_s, j_ic2)
-
+    ax.plot(nu_s, j_ic_para,label='PARAMO')
+    ax.plot(nu_s, j_ic)
+    ax.legend()
     plt.show()
