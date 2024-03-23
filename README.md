3031dcd96fec1f7b3576722d61334a3b0c2b57a3
zkdavis <zachkdavis00@gmail.com>
2024-03-23 02:38:01 -0400

    have fp numg test working. need to finish time convergence

--- README.md
+++ README.md
@@ -1,55 +1,3 @@
-# Functions
-### Rust Functions
-
-- **ic_iso_monochrome** - computes the emissivity($\frac{ergs}{cm^3 Sr}$) at frequency nuout(Hz) from inverse Compton (IC) scattering in an isotropic photon field assuming the photonfield is monochromatic [see in [src/radiation.rs:L356](https://github.com/zkdavis/PyParamo/blob/master/src/radiation.rs#L356)]
-  - **Parameters:**
-    - `nuout` (*f64*): frequency(Hz) in the comoving frame to compute emission at.
-    - `uext` (*f64*): energy density($\frac{ergs}{cm^-3}$) of the external photon field in the comoving frame.
-    - `nuext` (*f64*): frequency(Hz) in the comoving frame of the external photon field.
-    - `n` (*&Array1<f64>*): particle distribution as function of lorentz factor
-    - `g` (*&Array1<f64>*): Lorentz factor grid
-  - **Returns:**
-    - `jnu` (*f64*): emissivity($\frac{ergs}{cm^3 Sr}$) for frequency nuout
-
-
-- **ic_iso_monochrome_full** - computes the emissivity($\frac{ergs}{cm^3 Sr}$) from inverse Compton (IC) scattering in an isotropic photon field assuming the photonfield is monochromatic [see in [src/radiation.rs:L406](https://github.com/zkdavis/PyParamo/blob/master/src/radiation.rs#L406)]
-  - **Parameters:**
-    - `freqs` (*&Array1<f64>*): frequency(Hz) array in the comoving frame to compute emission over.
-    - `uext` (*f64*): energy density($\frac{ergs}{cm^-3}$) of the external photon field in the comoving frame.
-    - `nuext` (*f64*): frequency(Hz) in the comoving frame of the external photon field.
-    - `n` (*&Array1<f64>*): particle distribution as function of lorentz factor
-    - `g` (*&Array1<f64>*): Lorentz factor grid
-  - **Returns:**
-    - `jic` (*Array1<f64>*): emissivity($\frac{ergs}{cm^3 Sr}$) for frequency range freq
-
-
-- **rad_cool_pwl** - computes the radiative inverse Compton cooling ($\frac{\partial g}{\partial t}$ [$s^{-1}$]) from isotropic photon field uu($\frac{ergs}{cm^-3}$) [see in [src/radiation.rs:L502](https://github.com/zkdavis/PyParamo/blob/master/src/radiation.rs#L502)]
-  - **Parameters:**
-    - `gg` (*&Array1<f64>*): Lorentz factor grid
-    - `freqs` (*&Array1<f64>*): frequency(Hz) array in the comoving frame.
-    - `uu` (*&Array1<f64>*): energy density($\frac{ergs}{cm^-3}$) of photon field in the comoving frame for every frequency in freqs.
-    - `with_kn` (*bool*): bool that will include Klein Nishina affects to the cross section when true.
-  - **Returns:**
-    - `dotg` (*Array1<f64>*): radiative cooling ($\frac{\partial g}{\partial t}$ [$s^{-1}$])
-
-
-- **rad_cool_mono** - computes the radiative inverse Compton cooling ($\frac{\partial g}{\partial t}$ [$s^{-1}$]) from isotropic monotonic photon field u0($\frac{ergs}{cm^-3}$) [see in [src/radiation.rs:L557](https://github.com/zkdavis/PyParamo/blob/master/src/radiation.rs#L557)]
-  - **Parameters:**
-    - `gg` (*&Array1<f64>*): Lorentz factor grid
-    - `nu0` (*f64*): frequency(Hz) in the comoving frame of the photon field u0.
-    - `u0` (*f64*): energy density($\frac{ergs}{cm^-3}$) of photon field in the comoving frame.
-    - `with_kn` (*bool*): bool that will include Klein Nishina affects to the cross section when true.
-  - **Returns:**
-    - `dotg` (*Array1<f64>*): radiative cooling ($\frac{\partial g}{\partial t}$ [$s^{-1}$])
-
-### Python Functions
-
-- **power_law** - Computes a power law function. [see in [pysrc/paramo/misc_func.py:L33](https://github.com/zkdavis/PyParamo/blob/master/pysrc/paramo/misc_func.py#L33)]
-  - **Parameters:**
-  - **Returns:**
-    - `f` (**): Array of output values according to the power law.f = np.zeros_like(g)bounds = (g >= g_min) & (g <= g_max)f[bounds] = np.power(g, p)[bounds]if(normalize):f = f/np.trapz(f,g)return f*n0
-
-
 # Requirements
 
 The following Python packages are used in this project:
@@ -65,3 +13,4 @@ The following Python packages are used in this project:
 #### Python Version
 
 This project requires Python ^3.9.
+# Functions
--- comparisons/fokker_planck_test.py
+++ comparisons/fokker_planck_test.py
@@ -1,10 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from paramo import misc_func
+import paramo as para
+
+
+def analytical_form(g):
+    C0 = 3.48e-11
+    tesc = 1e0 / (C0 * (10 ** (4.5)))
+    return (g ** 2) * np.exp(-2 * C0 * tesc * (g - 1))
+
+
+def analytical_solution(n0, g):
+    integrand_values = analytical_form(g)
+    integral_approx = np.trapz(integrand_values, g)
+    x = n0 / integral_approx
+    return x * analytical_form(g)
 
 
 def get_n0(p, gmin, gmax):
-    x_values = np.linspace(gmin, gmax, num=1000)
-    y_values = x_values**p
+    x_values = np.logspace(np.log10(gmin), np.log10(gmax), num=1000)
+    y_values = x_values ** p
     return np.trapz(y_values, x_values)
 
 
@@ -37,29 +52,59 @@ def adjust_plot(ax, x_label, y_label, title):
     ax.tick_params(axis='both', which='both', labelsize=15)
 
 
-def process_data(numt, numgs, n0):
-    xs, ys, gs, ns, nios, eers = [], [], [], [], [], []
+def get_error_analytic(ei, eig, n0):
+    ef = analytical_solution(n0, eig)
+    valid_ef = ef > 0
+    errors = np.where(valid_ef, ((ef - ei) / ef) ** 2, 0)
+    er = np.sqrt(sum(errors) / len(eig))
+    eers = np.sqrt(errors)
+    return er, eers
+
+
+def run_test(numg, numt, n0):
+    C0 = 3.48e-11
+    t_acc = 1 / (C0 * (10 ** (4.5)))
+    tmax = t_acc * 300
+    t = np.logspace(0, np.log10(tmax), numt)
+    g = np.logspace(0, np.log10(1.5e8), numg)
+    D_0 = 0.5 * np.power(g, 2) / t_acc
+    D1 = 2 * D_0
+    gdot1 = np.zeros([numt, numg])
+    gdot1[0, :] = C0 * np.power(g, 2) - 4 * D_0 / g
+    tlc = tmax
+    p, gcut1, g2cut1 = 0, 1e4, 1e6
+    n1 = np.zeros([numt, numg])
+    n1[0, :] = misc_func.power_law(1, g, p, gcut1, g2cut1)
+    n1[0, :] = n0 * n1[0, :] / np.trapz(n1[0, :], g)
+
+    for i in range(1, numt):
+        dt = t[i] - t[i - 1]
+        n1[i, :] = para.fp_findif_difu(dt, g, n1[i - 1, :], gdot1[i - 1, :], D1, np.zeros_like(D1), 1e200, tlc, False)
+        gdot1[i, :] = gdot1[0, :]
+
+    return g, n1
+
+
+def process_data(numt, numgs):
+    xs, ys, gs, g2s, ns, nios, n0s, eers = [], [], [], [], [], [], [], []
+    n0 = get_n0(0, 1e4, 1e6)
     for i, mg in enumerate(numgs):
-        eissr = get_convergence_results(numt, mg)
-        ei, eig = eissr.n4[:, -1], eissr.g
-        nio = np.trapz(ei, eig) / np.trapz(eissr.n4[:, 0], eig)
-        nios.append(nio)
-        gminci, gmaxci = np.argmin(np.abs(eig - 1e0)), np.argmin(np.abs(eig - 1e7))
-        eig, ei = eig[gminci:gmaxci], ei[gminci:gmaxci]
-        _, eer = get_error_analytic(ei, eig, n0)
-        xs.append(len(eig))
-        gs.append(eig)
-        ns.append(ei)
+        g, n = run_test(mg, numt, n0)
+        gminci, gmaxci = np.argmin(np.abs(g - 1e1)), np.argmin(np.abs(g - 1e7))
+        g2, n2 = g[gminci:gmaxci], n[-1, gminci:gmaxci]
+        er, eer = get_error_analytic(n2, g2, n0)
+        xs.append(mg)
+        gs.append(g)
+        ns.append(n)
         eers.append(eer)
-        ys.append(_)
-    return xs, ys, gs, ns, nios, eers
+        ys.append(er)
+        n0s.append(n0)
+        g2s.append(g2)
+    return xs, ys, gs, g2s, ns, n0s, eers
 
 
 def convergence_plots_analytic(numt, numgs):
-    p = 0e0
-    gmin, gmax = 1e4, 1e6
-    n0 = get_n0(p, gmin, gmax)
-    xs, ys, gs, ns, nios, eers = process_data(numt, numgs, n0)
+    xs, ys, gs, g2s, ns, n0s, eers = process_data(numt, numgs)
 
     # Setup plots
     fig_error, ax_error = plot_setup()
@@ -68,17 +113,119 @@ def convergence_plots_analytic(numt, numgs):
     fig_nios, ax_nios = plot_setup()
     ax_nios.set_xscale('linear')
 
-    # Plot data
-    plot_data(ax_error, xs, ys, 'scatter')
+    xc2 = np.logspace(1.5, 3.8)
+    ax_error.plot(xc2, ys[2] * (xc2 / xs[2]) ** -1, label='$\propto N^{-1}$', color='black')
+
+    plot_data(ax_error, numgs, ys, 'scatter')
     for i in range(len(gs)):
-        plot_data(ax_eers, gs[i], eers[i], 'scatter', s=4)
-        plot_data(ax_ns, gs[i], ns[i], 'plot', label='N=' + str(xs[i]))
-    ax_ns.plot(gs[-1], analytical_solution(n0, gs[-1]), '--')
+        plot_data(ax_eers, g2s[i], eers[i], 'scatter', s=4)
+        plot_data(ax_ns, gs[i], ns[i][-1, :], 'plot', label='N=' + str(numgs[i]))
+    ax_ns.plot(gs[-1], analytical_solution(n0s[-1], gs[-1]), '--')
 
     adjust_plot(ax_error, "Number of bins (N)", "Error", "Convergence at steady state")
     adjust_plot(ax_ns, "$\gamma$", r"n $[cm^{-3}]$", "Steady State Solutions")
     adjust_plot(ax_eers, "$\gamma$", "Error", "Error Analysis")
-    adjust_plot(ax_nios, "Number of bins (N)", "Normalized Intensity", "Intensity Over Bins")
+    # adjust_plot(ax_nios, "Number of bins (N)", "Normalized Intensity", "Intensity Over Bins")
 
     add_legend(ax_ns)
     plt.show()
+
+def run_time_test(numg,numt,n0):
+    C0 = 3.48e-11
+    t_acc = 1 / (C0 * (10**(4.5)))
+    tmax = t_acc * 300
+    t = np.logspace(0, np.log10(tmax), numt)
+    g = np.logspace(0, np.log10(1.5e8), numg)
+    D_0 = 0.5 * np.power(g, 2) / t_acc
+    D1 = 2 * D_0
+    gdot1 = np.zeros([numt, numg])
+    gdot1[0, :] = C0 * np.power(g, 2) - 4 * D_0 / g
+    tlc = tmax
+    p, gcut1, g2cut1 = 0, 1e4, 1e6
+    n1 = np.zeros([numt, numg])
+    n1[0, :] = misc_func.power_law(1, g, p, gcut1, g2cut1)
+    n1[0, :] = n0*n1[0, :]/np.trapz(n1[0, :],g)
+
+    for i in range(1, numt):
+        dt = t[i] - t[i - 1]
+        n1[i, :] = para.fp_findif_difu(dt, g, n1[i - 1, :], gdot1[i - 1, :], D1, np.zeros_like(D1), 1e200, tlc, False)
+        gdot1[i, :] = gdot1[0, :]
+
+    return g,n1,t
+
+
+def get_error_analytic_time(ei, ef):
+    errors = np.where(ef > 0, ((ef - ei) / ef) ** 2, 0)
+    eers = np.sqrt(errors)
+    er = np.sqrt(np.mean(errors))
+
+    return er, eers
+
+def plot_convergence_results(numts, numg, t):
+    results = []
+    nios = []
+    gmin, gmax = 1e4, 1e6
+    n0 = get_n0(0, gmin, gmax)
+    ts = [5e-4, 1e-3, 5e-3, 1e-2]
+
+    for mt in numts:
+        g,n,tr = run_time_test(mt, numg)
+        tind = np.argmin(np.abs(tr - t))
+        ei, eig = n[tind,:], g
+        ef = n0 * np.array([para.eq_59_Park1995(t + 1e-4, g) for g in eig]) / np.trapz(para.eq_59_Park1995(1e-4, eig), eig)
+        nio = np.trapz(ei, eig) / np.trapz(n[0,:], eig)
+        nios.append(nio)
+        minargs = np.argsort(np.abs(ei - 1e-8))
+        ma1 = minargs[0]
+        ma2 = minargs[1]
+        for k in range(len(minargs)):
+            if (np.abs(minargs[k] - ma1) > 10):
+                ma2 = minargs[k]
+                break
+        if (ma1 > ma2):
+            gmaxci = ma1
+            gminci = ma2
+        else:
+            gmaxci = ma2
+            gminci = ma1
+
+        eig = eig[gminci:gmaxci]
+        ei = ei[gminci:gmaxci]
+        ef = ef[gminci:gmaxci]
+
+        er, eer = get_error_analytic_time(ei, ef, eig)
+        results.append((mt, er, eer, eig, ei))
+
+    plot_results(results, ts[-1], nios)
+
+
+def plot_results(results, t, nios):
+    fig, axs = plt.subplots(4, 1, figsize=(10, 20))
+    colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
+
+    for i, (mt, er, eer, eig, ei) in enumerate(results):
+        axs[0].scatter(mt, er, color=colors[i % len(colors)], label=f'MT={mt}')
+        axs[1].scatter(eig, eer, color=colors[i % len(colors)], s=4)
+        axs[2].plot(eig, ei, label=f'Number of bins: {mt}')
+        axs[3].scatter(mt, nios[i], color=colors[i % len(colors)])
+
+    for ax in axs[:3]:
+        ax.set_yscale('log')
+        ax.set_xscale('log')
+        ax.legend()
+
+    axs[3].set_xscale('log')
+    axs[0].set_title(f"Convergence at t={t:.2E}")
+    axs[2].plot(results[-1][3], results[-1][2], '--', label='Analytic solution')
+    axs[2].set_ylim([1e-8, 1e-1])
+    axs[2].set_xlim([1e1, 1e4])
+    plt.show()
+
+
+if __name__ == '__main__':
+    garr = [25, 50, 100, 200, 400, 800, 1600, 3200]
+    # garr = [100, 500, 3500]
+    # garr = [20,50, 100,200,300,500]
+    numtarr = [600]
+    # numtarr = [200]
+    convergence_plots_analytic(numtarr[0], garr)
